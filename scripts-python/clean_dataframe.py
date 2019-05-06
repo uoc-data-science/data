a89837374fa84c3f4f86e7a5be24b1245a215da0
Laurenz <laurenzl33@hotmail.de>
2019-05-06 15:10:22 +0200

    generic Clean order_data and clickstream_data

--- scripts-python/clean_dataframe.py
+++ scripts-python/clean_dataframe.py
@@ -23,25 +23,41 @@ def delete_columns_with_nullonly_or_morethan99p(df):
     df = drop_column_from_list(df,drop_column_list)
     print(df)
     return df
-        
+
 
 def drop_column_from_list(df,droplist):
     for row in droplist:
         df = df.drop(columns=row[0])   
     return df
-    
 
-        
+
+def delete_empty_indexorcolumns_in_df(df,index_or_columns,threshold):
+    df = df.dropna(axis=index_or_columns, thresh=threshold)
+    return df
+
+
+def clean_df_generic(df,threshold):
+    print(df)
+    index_or_columns = "columns"
+    df = delete_empty_indexorcolumns_in_df(df,index_or_columns,threshold)
+    index_or_columns = "index"
+    df = delete_empty_indexorcolumns_in_df(df,index_or_columns,threshold)
+    print(df)
+    return df
+
+   
+
         
 
 
 
 
 df = read_data.load_dataframe("order.pkl")
-delete_columns_with_nullonly_or_morethan99p(df)
+df = clean_df_generic(df,1)
+
 
 df2 = read_data.load_dataframe("click.pkl")
-delete_columns_with_nullonly_or_morethan99p(df2)
+df2 = clean_df_generic(df2,1)
 
 
 
