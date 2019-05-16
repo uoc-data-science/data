f61bdcf3a2f010ef10108d261e1751ba7d7ffede
phyk <ppeter1@smail.uni-koeln.de>
2019-05-16 19:35:17 +0200

    Kleine Rechtschreib und Styling Aenderungen. Ausserdem merge der clickstream Daten

--- scripts-python/check_dfs.py
+++
@@ -1,5 +0,0 @@
-from read_data import load_dataframe
-
-df = load_dataframe("order.pkl")
-df2 = load_dataframe("click.pkl")
-
--- scripts-python/initialize_data.py
+++ scripts-python/initialize_data.py
@@ -1,5 +1,6 @@
 from copy_open_zip import copy_into_raw_data_and_data_understanding
 from read_data import read_raw_data
+from read_data import save_dataframe
 from clean_dataframe import clean_df_generic
 
 # get raw_datasets
@@ -21,9 +22,15 @@ print(df_order.head(10))
 ####
 # create new csv with head and data
 # And create sample Excel sheet with 100 lines
-df_clickstream = read_raw_data("clickstream_data.csv","clickstream_columns.txt","clickstream.xlsx","click.pkl") 
+df_clickstream = read_raw_data("clickstream_data.csv", "clickstream_columns.txt",
+                               "clickstream.xlsx", "click.pkl")
+df_clickstream_part_2 = read_raw_data("clickstream_data_part_2.csv", "clickstream_columns.txt",
+                                      "clickstream2.xlsx", "click2.pkl")
+df_clickstream = df_clickstream.append(df_clickstream_part_2)
+save_dataframe(df_clickstream, "click_all.pkl")
+
 # clean
-df_clickstream = clean_df_generic(df_clickstream,0)
+df_clickstream = clean_df_generic(df_clickstream, 0)
 
 
 print(df_clickstream.head(10))
