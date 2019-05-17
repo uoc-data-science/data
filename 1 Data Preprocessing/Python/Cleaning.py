32a3fe6eaff09fe975481d441eca16237b87b6f5
cangaroo6 <kimferres@gmail.com>
2019-05-17 19:35:06 +0200

    Added second clicks file to cleaning

--- 1 Data Preprocessing/Python/Cleaning.py
+++ 1 Data Preprocessing/Python/Cleaning.py
@@ -9,6 +9,7 @@ pathOrdersSmall = "../../0 Data/order_data_small_P.csv"
 pathOrdersHeaders = "../../orders/order_columns.txt"
 
 pathClicks = "../../clickstream/clickstream_data.csv"
+pathClicks2 = "../../clickstream/clickstream_data_part_2.csv"
 newPathClicks = "../../0 Data/clickstream_data_P.csv"
 pathClicksClean = "../../0 Data/clickstream_data_cleaned_P.csv"
 pathClicksSmall = "../../0 Data/clickstream_data_small_P.csv"
@@ -41,6 +42,8 @@ ordersHeaders = headers(pathOrdersHeaders)
 
 # Read data
 clicks = pd.read_csv(pathClicks, sep=",", names=clicksHeaders, dtype=str, encoding="ISO-8859-1")
+clicks2 = pd.read_csv(pathClicks2, sep=",", names=clicksHeaders, dtype=str, encoding="ISO-8859-1")
+clicks = clicks.append(clicks2)
 clicks = clicks.replace(to_replace="?", value=nan)
 clicks = clicks.replace(to_replace="NULL", value=nan)
 orders = pd.read_csv(pathOrders, sep=",", names=ordersHeaders, dtype=str, encoding="utf-8")
@@ -68,7 +71,7 @@ orders.to_csv(path_or_buf=pathOrdersClean, index=False)
 print("Orders:" + str(orders.shape))
 
 #Merging
-mergedData = pd.merge(clicks, orders,  how='inner', left_on=['Session ID','Product Object ID','Product ID'], right_on = ['Order Session ID','Product Object ID','Product ID'])
+mergedData = pd.merge(clicks, orders,  how='inner', left_on=['Session ID'], right_on=['Order Session ID'])
 print(list(mergedData.columns.values))
 print(list(mergedData.shape))
 mergedData.to_csv(path_or_buf=pathMerge, index=False)
