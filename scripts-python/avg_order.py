af4465739478dd954cc8e6adacf9337487c691b9
phyk <ppeter1@smail.uni-koeln.de>
2019-05-17 17:44:16 +0200

    Generate Average Order

--- scripts-python/avg_order.py
+++ scripts-python/avg_order.py
@@ -1,30 +1,24 @@
 from tabulate import tabulate
 from read_data import load_dataframe
 import clean_dataframe
+import numpy
 
 df = load_dataframe("order.pkl")
-df2 = load_dataframe("click.pkl")
 
 clean_dataframe.clean_df_generic_alternative(df)
-clean_dataframe.clean_df_generic_alternative(df2)
 
 clean_dataframe.drop_identical(df)
-clean_dataframe.drop_identical(df2)
 
 
 # df_mod["order_datetime"] = pd.to_datetime(df['Order Line Date'] + ' ' + df['Order Line Date_Time'], format="%Y-%m-%d %H\\:%M\\:%S")
 
-f = open("../tmp_outputs/consoleWODupl.txt", "w")
+f = open("../tmp_outputs/avgOrder.txt", "w")
 
-f.write(str(df.shape)+" Shape of Order Data\r\n")
-f.write(tabulate(df.describe(include='all'), headers=[""]+df.T.index, tablefmt='psql'))
-f.write("\r\n")
-f.write(tabulate(df.head(5), headers=[""]+df.T.index, tablefmt='psql'))
-f.write("\r\n")
-f.write(str(df2.shape)+" Shape of Clickstream Data\r\n")
-f.write(tabulate(df2.describe(include='all'), headers=[""]+df2.T.index, tablefmt='psql'))
-f.write("\r\n")
-f.write(tabulate(df2.head(5), headers=[""]+df2.T.index, tablefmt='psql'))
+sum1 = df.describe()
+sum2 = df.describe(include=[numpy.object, numpy.bool])
+summary = sum1.iloc[1].append(sum2.iloc[2])
+summary.to_csv("AverageOrder.txt")
+
 # f.write("order\r\n")
 # for item in find_identical(df):
 #     f.write("\""+item+"\" ")
