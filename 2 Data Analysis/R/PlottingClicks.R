30708e21b97c9983c1dec6fd4926e9b24b6a7a6e
Unknown <buggitheclown@web.de>
2019-05-21 11:13:36 +0200

    Added source for multiplot function in plottingClicks

--- 2 Data Analysis/R/PlottingClicks.R
+++ 2 Data Analysis/R/PlottingClicks.R
@@ -79,7 +79,7 @@ summary[is.na(summary)]<-""
 
 #-----------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------
-# Utility function for subplot support
+# Utility function for subplot support, Source: http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
 multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
   library(grid)
   # Make a list from the ... arguments and plotlist
