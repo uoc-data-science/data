c94d510ce252afa7e944d6f5578fcc35edbeb076
Unknown <buggitheclown@web.de>
2019-06-10 14:47:39 +0200

    Inference added to report

--- 2 Data Analysis/R/ExperimentInferenceAnalysis.R
+++ 2 Data Analysis/R/ExperimentInferenceAnalysis.R
@@ -6,7 +6,7 @@ experiment <- read.csv(file=pathExperiment)
 
 # Reformat the results to allow inference analysis
 experimentDf <- data.frame(matrix(ncol = 3, nrow = 0))
-x <- c("Sales_in_EUR","Used_Profit_Oriented_Reccomendations","Used_Top_Reccomendations")
+x <- c("Sales_in_EUR","Used_Profit_Oriented_recommendations","Used_Top_recommendations")
 colnames(experimentDf) <- x
 
 for (row in 1:nrow(experiment)){
@@ -24,36 +24,36 @@ for (row in 1:nrow(experiment)){
   }
 }
 
-experimentDf[,"Used_Profit_Oriented_Reccomendations"] <- factor(experimentDf[,"Used_Profit_Oriented_Reccomendations"])
-experimentDf[,"Used_Top_Reccomendations"] <- factor(experimentDf[,"Used_Top_Reccomendations"])
+experimentDf[,"Used_Profit_Oriented_recommendations"] <- factor(experimentDf[,"Used_Profit_Oriented_recommendations"])
+experimentDf[,"Used_Top_recommendations"] <- factor(experimentDf[,"Used_Top_recommendations"])
 experiment <- experimentDf
 
 # Do inference analysis
-# Part 1: Test if the profit oriented reccomendation system causes a significant difference in sales
+# Part 1: Test if the profit oriented recommendation system causes a significant difference in sales
 
 obs_stat <- experiment %>% 
-  filter(Used_Top_Reccomendations == 0) %>%
-  specify(Sales_in_EUR ~ Used_Profit_Oriented_Reccomendations) %>%
+  filter(Used_Top_recommendations == 0) %>%
+  specify(Sales_in_EUR ~ Used_Profit_Oriented_recommendations) %>%
   calculate(stat = "diff in means", order=c("1","0"))
 
 permuted_stat <- experiment %>% 
-  filter(Used_Top_Reccomendations == 0) %>%
-  specify(Sales_in_EUR ~ Used_Profit_Oriented_Reccomendations) %>%
+  filter(Used_Top_recommendations == 0) %>%
+  specify(Sales_in_EUR ~ Used_Profit_Oriented_recommendations) %>%
   hypothesize(null = "independence") %>%
   generate(reps = 1000, type="permute") %>%
   calculate(stat = "diff in means", order=c("1","0"))
 
 p_val <- permuted_stat %>%
   get_p_value(obs_stat = obs_stat, direction = "two_sided")
-print(p_val)
+print(paste("p-value = ",as.data.frame(p_val)[1,1],sep=""))
 
 viz_plot <- permuted_stat %>% visualize()
 viz_plot <- viz_plot + shade_p_value(obs_stat, direction = "two_sided", color = "black", fill = "grey")
 print(viz_plot)
 
 bootstrap_stat <- experiment %>%
-  filter(Used_Top_Reccomendations == 0) %>%
-  specify(Sales_in_EUR ~ Used_Profit_Oriented_Reccomendations) %>%
+  filter(Used_Top_recommendations == 0) %>%
+  specify(Sales_in_EUR ~ Used_Profit_Oriented_recommendations) %>%
   generate(reps = 1000, type = "bootstrap") %>%
   calculate(stat = "diff in means", order = c("1", "0"))
 
@@ -66,31 +66,31 @@ viz_plot <- bootstrap_stat[complete.cases(bootstrap_stat), ] %>% visualize()
 viz_plot <- viz_plot + shade_confidence_interval(ci, color = "black", fill = "grey")
 print(viz_plot)
 
-# Part 2: Test if the popular products reccomendation system causes a significant difference in sales
+# Part 2: Test if the popular products recommendation system causes a significant difference in sales
 
 obs_stat <- experiment %>% 
-  filter(Used_Profit_Oriented_Reccomendations == 0) %>%
-  specify(Sales_in_EUR ~ Used_Top_Reccomendations) %>%
+  filter(Used_Profit_Oriented_recommendations == 0) %>%
+  specify(Sales_in_EUR ~ Used_Top_recommendations) %>%
   calculate(stat = "diff in means", order=c("1","0"))
 
 permuted_stat <- experiment %>% 
-  filter(Used_Profit_Oriented_Reccomendations == 0) %>%
-  specify(Sales_in_EUR ~ Used_Top_Reccomendations) %>%
+  filter(Used_Profit_Oriented_recommendations == 0) %>%
+  specify(Sales_in_EUR ~ Used_Top_recommendations) %>%
   hypothesize(null = "independence") %>%
   generate(reps = 1000, type="permute") %>%
   calculate(stat = "diff in means", order=c("1","0"))
 
 p_val <- permuted_stat %>%
   get_p_value(obs_stat = obs_stat, direction = "two_sided")
-print(p_val)
+print(paste("p-value = ",as.data.frame(p_val)[1,1],sep=""))
 
 viz_plot <- permuted_stat %>% visualize()
 viz_plot <- viz_plot + shade_p_value(obs_stat, direction = "two_sided", color = "black", fill = "grey")
 print(viz_plot)
 
 bootstrap_stat <- experiment %>%
-  filter(Used_Profit_Oriented_Reccomendations == 0) %>%
-  specify(Sales_in_EUR ~ Used_Top_Reccomendations) %>%
+  filter(Used_Profit_Oriented_recommendations == 0) %>%
+  specify(Sales_in_EUR ~ Used_Top_recommendations) %>%
   generate(reps = 1000, type = "bootstrap") %>%
   calculate(stat = "diff in means", order = c("1", "0"))
 
