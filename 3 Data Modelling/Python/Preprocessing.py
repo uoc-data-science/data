88221ed0193e88c1aeb99e159e8281e01490fd65
Unknown <buggitheclown@web.de>
2019-06-29 14:36:11 +0200

    Prediction Decision Tree Code

--- 3 Data Modelling/Python/Preprocessing.py
+++ 3 Data Modelling/Python/Preprocessing.py
@@ -3,65 +3,49 @@ import datetime
 import numpy as np
 
 pathClicks = '../../0 Data/clickstream_data_cleaned_P.csv'
-pathMerge = "../../0 Data/merged_P.csv"
-pathCustomers = "../customers.csv"
-pathClickCustomer1 = '../../4 Data Overview/Tables/ ClickstreamDataCustomer_Factors.csv'
-pathClickCustomer2 = '../../4 Data Overview/Tables/ ClickstreamDataCustomer_Numerical.csv'
+pathResult = "../prediction_input_data.csv"
 
 #############################################################################
 
 # read data
 clicksOrig = pd.read_csv(pathClicks, sep=",", dtype=str)
-merge = pd.read_csv(pathMerge, sep=",", dtype=str)
-clickCustomer1 = pd.read_csv(pathClickCustomer1, sep=",", dtype=str)
-clickCustomer2 = pd.read_csv(pathClickCustomer2, sep=",", dtype=str)
 
-# build subset on Customer Columns
-clickCustomer1 = [w.replace('.', ' ') for w in clickCustomer1['Variable']]
-clickCustomer1 = [w.replace('   ', '\. ') for w in clickCustomer1]
-clickCustomer2 = [w.replace('.', ' ') for w in clickCustomer2['Variable']]
-clickCustomer2 = [w.replace('   ', '\. ') for w in clickCustomer2]
-clickCustomer1.extend(clickCustomer2)
-clickCustomer1.extend(['REQUEST_DAY_OF_WEEK','REQUEST_HOUR_OF_DAY', 'Customer ID'])
-clicks = ((clicksOrig[clickCustomer1]).drop_duplicates(subset=['Customer ID']))[pd.notnull(clicksOrig['Customer ID'])]
-
-#calculate session duration and click number
-clicksOrig = clicksOrig[pd.notnull(clicksOrig['Customer ID'])]
-df = pd.DataFrame(columns=['Customer ID','Clicks','Duration'])
-maxClick = 0
+df = pd.DataFrame(columns=['Clicks','Duration_in_Seconds','REQUEST_DAY_OF_WEEK','REQUEST_HOUR_OF_DAY','Ordered'])
+clickCount = '1'
 minTime = 0
 maxTime = 0
-customerID = 0
+ordered = "No"
+init = 1
 for index, row in clicksOrig.iterrows():
     if row["Request Sequence"] == '1':
-        if maxClick != 0:
-            duration = maxTime - minTime
-            df.loc[len(df)] = [str(customerID), str(maxClick), str(duration)]
+        if init == 0 and clickCount != '1':
+            duration = (maxTime - minTime).total_seconds()
+            df.loc[len(df)] = [str(clickCount), str(duration), str(day), str(hour), str(ordered)]
+        if init == 1:
+            init = 0
+        ordered = "No"
         #new minTime
         dt = row['Request Date'] + ' ' + row['Request Date_Time']
         minTime = datetime.datetime.strptime(dt, '%Y-%m-%d %H:%M:%S')
-    customerID = row["Customer ID"]
-    maxClick = row["Request Sequence"]
+        day = row["REQUEST_DAY_OF_WEEK"]
+        hour = row["REQUEST_HOUR_OF_DAY"]
+    if row["Request Template"] == "checkout/thankyou\.jhtml":
+        ordered = "Yes"
+    clickCount = row["Request Sequence"]
     dt = row['Request Date'] + ' ' + row['Request Date_Time']
     maxTime = datetime.datetime.strptime(dt, '%Y-%m-%d %H:%M:%S')
-df = df.drop_duplicates(subset=['Customer ID'])
-clicks = pd.merge(clicks, df, how='left', left_on=['Customer ID'], right_on=['Customer ID'])
 
-# read all customers who ordered
-oCustomers = merge['Customer ID'].unique()
 
-# mark customers who ordered
-orderer = []
-for index, row in clicks.iterrows():
-    if row['Customer ID'] in oCustomers:
-        orderer.append(1)
-    else:
-        orderer.append(0)
-
-# save dataset
-clicks["Orderer"] = orderer
-clicks.to_csv(pathCustomers, index=False)
-
+# one hot encode where needed
+df['REQUEST_HOUR_OF_DAY'] = df['REQUEST_HOUR_OF_DAY'].apply(lambda x: "{}{}".format('Hour_', x))
+one_hot = pd.get_dummies(df['REQUEST_HOUR_OF_DAY'])
+df = df.drop('REQUEST_HOUR_OF_DAY',axis = 1)
+df = df.join(one_hot)
+df['REQUEST_DAY_OF_WEEK'] = df['REQUEST_DAY_OF_WEEK'].apply(lambda x: "{}{}".format('Weekday_', x))
+one_hot = pd.get_dummies(df['REQUEST_DAY_OF_WEEK'])
+df = df.drop('REQUEST_DAY_OF_WEEK',axis = 1)
+df = df.join(one_hot)
+df.to_csv(pathResult, index=False)
 
 
 
