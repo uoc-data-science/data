e1fb5def8f38c361782362179ef25d81ae649ac0
Patrick Preuß <patrickp89@users.noreply.github.com>
2019-07-16 16:33:39 +0200

    More plots for final paper (#21)
    * added generated Bookdown files to gitignore
    
    * refactor: changed sections names, altered the order of code chunks
    
    * wip: added LC for Order Total distributeion; added subsection headings; TOC level is now 2!

--- src/misc/install-dependent-packages.r
+++ src/misc/install-dependent-packages.r
@@ -6,5 +6,7 @@ install.packages("tidyverse", repos='http://cran.uni-muenster.de/', dependencies
 install.packages("caret", repos='http://cran.uni-muenster.de/', dependencies=TRUE)
 install.packages("rpart.plot", repos='http://cran.uni-muenster.de/', dependencies=TRUE)
 install.packages("kableExtra", repos='http://cran.uni-muenster.de/', dependencies=TRUE)
+install.packages("usmap", repos='http://cran.uni-muenster.de/', dependencies=TRUE)
 
 update.packages(repos='http://cran.uni-muenster.de/', dependencies=TRUE, ask=FALSE, checkBuilt=TRUE)
+
