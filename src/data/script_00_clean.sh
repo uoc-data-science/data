5f512d252d13e47636b4223a235bec23e830000d
Patrick Preuß <patrickp89@users.noreply.github.com>
2019-07-24 13:59:57 +0200

    Minor refactoring (#26)
    * added a zoomed in partial view of the decision tree
    
    * refactor: made some changes to the first chapter
    
    * style: all figures are now centered!
    
    * fix: unzipping now also workz under windows...

--- src/data/script_00_clean.sh
+++ src/data/script_00_clean.sh
@@ -6,6 +6,8 @@ rm -rf data/interim/joined/*
 rm -f notebooks/*.tex
 rm -f notebooks/*.pdf
 rm -f notebooks/*.html
+rm -f notebooks/*.log
+rm -f notebooks/*.csv
 rm -rf notebooks/product-stats_files/
 rm -f reports/figures/*
-
+rm -f Rplots.pdf
